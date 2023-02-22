2e087b37f0136dab15ddbe0cbb8dfc074055cdde
Ali Korloo <korloo@outlook.com>
2023-02-22 20:34:47 +0330

    migration to postgresql (via sqlalchemy)

--- config/__init__.py
+++ config/__init__.py
@@ -11,28 +11,38 @@ db_path = Path(__file__).parent.parent / 'db' / 'catalogue.db'
 with config_path.open(mode='rb') as file:
     configs = tomli.load(file)
 # return tomli.load(file)
+# maybe later: getting item off of cfg= cfg.get(key1, key2,...)
 
 def write(conf:dict):
     with config_path.open(mode='wb') as file:
         tomli_w.dump(conf, file)
 
 if not configs: # if not load():
-    configs =  {'last_update': {
-                    'instrument': 0, 
-                    'capital_increase': 0,
-                    'instrument_type': 0,
-                    'daily_quotes': 0},
-                'path': {
-                    'config_file': str(config_path),
-                    'db': str(db_path)},
-                'db_health': {
-                    'last_check_time':'datetime -not yet implemented',
-                    'last_check_status':'ok/faulty -not yet implemented'},
-                'URI': {
-                    'DAILY_PRICES_HISTROY_TO_DATE':'http://service.tsetmc.com/tsev2/data/TseClient2.aspx?t=ClosingPrices&a={}',
-                    'DAILY_PRICES_LAST_N_DAYS':'http://www.tsetmc.com/tsev2/data/InstTradeHistory.aspx?i={}&Top={}&A={}',
-                    'TRADE_DETAILS_HISTORY':'http://cdn.tsetmc.com/api/Trade/GetTradeHistory/{}/{}/{}',
-                    'TRADE_DETAILS_CURRENT_DAY':'http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i={}',
-                    'CLIENT_TYPE_HISTORY':'http://cdn.tsetmc.com/api/ClientType/GetClientTypeHistory/{}/{}',
-                    'SHAREHOLDER_HISTORY':'http://cdn.tsetmc.com/api/Shareholder/{}/{}'}}
+    configs =  {
+        'LAST_UPDATE': {
+            'INSTRUMENTS': 0, 
+            'CAPITAL_INCREASE': 0,
+            'INSTRUMENT_TYPES': 0,
+            'DAILY_QUOTES': 0
+        },
+        'PATH': {
+            'CONFIG_FILE': str(config_path),
+            'db': str(db_path)
+        },
+        'URI': {
+            'DAILY_PRICES_HISTROY_TO_DATE':'http://service.tsetmc.com/tsev2/data/TseClient2.aspx?t=ClosingPrices&a={}',
+            'DAILY_PRICES_LAST_N_DAYS':'http://www.tsetmc.com/tsev2/data/InstTradeHistory.aspx?i={}&Top={}&A={}',
+            'TRADE_DETAILS_HISTORY':'http://cdn.tsetmc.com/api/Trade/GetTradeHistory/{}/{}/{}',
+            'TRADE_DETAILS_CURRENT_DAY':'http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i={}',
+            'CLIENT_TYPE_HISTORY':'http://cdn.tsetmc.com/api/ClientType/GetClientTypeHistory/{}/{}',
+            'SHAREHOLDER_HISTORY':'http://cdn.tsetmc.com/api/Shareholder/{}/{}'
+        },
+        'DB':{
+            'HOST': '127.0.0.1',
+            'NAME': '',
+            'USERNAME': '',
+            'PASSWORD': '',
+            # 'HEALTH': 'STATUS@DATETIME' # later
+        }
+    }
     write(configs)
--- main.py
+++ main.py
@@ -1,37 +1,18 @@
 import config 
 from data_ops import (
     latest_workday, 
-    update_daily_quotes, 
-    update_instruments_and_capital_increase, 
-    update_instrument_types,
-    update_instruments_info)
+    update_db,
+    # update_daily_quotes, 
+    # update_instruments_info_and_share_increase, 
+    # update_instrument_types,
+    # update_instruments_info
+)
 
 
 cfg = config.configs 
-print('Latest Workday: {}\nLast Database Update: {}'.format(latest_workday,cfg['last_update']['instrument']))
+print('Latest Workday: {}\nLast Database Update: {}'.format(latest_workday,cfg['LAST_UPDATE']['INSTRUMENTS']))
 
 # Main.py
-# Update Catalogue
-if cfg['last_update']['instrument'] == latest_workday:
-    print('Catalogue is already up to date.')
-elif cfg['last_update']['instrument'] < latest_workday:
-    update_instruments_and_capital_increase()
-    if not cfg['last_update']['instrument']:
-        update_instrument_types()
-        print('Catalogue Initialized.')
-    else:
-        print('Catalogue updated.')
-else:
-    print('Error: Invalid Configuration.')
-
-# Update Quotes
-if cfg['last_update']['daily_quotes'] == latest_workday:
-    print('Daily Quotes are already up to date.')
-elif cfg['last_update']['daily_quotes'] < latest_workday:
-    update_daily_quotes()
-    if not cfg['last_update']['daily_quotes']:
-        print('Daily timeframe Initialized.')
-    else:
-        print('Daily timeframe updated.')
-else:
-    print('Error: Invalid Configuration.')
+# region: Update Database
+update_db('instrument_types')
+# endregion
